4823f5f1ecc1ea16845389cb91c10ca18bbe8361
admln52 <ismrwang@gmail.com>
2016-12-15 12:35:56 +0800

    put data into localstorage

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 var USER = '52admln';
+var STORAGE_KEY = '52admin-projects';
+
+$(".js-refresh").on('click', function () {
+    console.log(1);
+    $(".loader").removeClass("hidden"); // 隐藏loader
+    $(".wrapper").removeClass("animated").addClass("hidden"); // 页面载入动画
+    getData();
+});
+
 
 function renderData(data) {
     var str = "";
@@ -9,27 +18,49 @@ function renderData(data) {
                 name = data[index].name,
                 desc = data[index].description || "暂无描述"; //如无描述,则显示暂无描述
 
-            str += '<li class="repo grid ' + (language.toString()).toLowerCase() + '">'
+            $(".loader").addClass("hidden"); // 隐藏loader
+            $(".wrapper").removeClass("hidden").addClass("animated"); // 页面载入动画
+
+            str += '<li class="repo grid ' + '">'
                 + '<a href="' + url + '" target="_blank">'
                 + '<h2>' + name + '</h2>'
                 + '<p>' + desc + '</p>'
                 + '</a>'
+                + '<div class="repo-tags">' + (language.toString()).toUpperCase() + '</div>'
                 + '</li>';
         }
     }
     $('.js-list').html(str);
 }
 
-function init() {
+function getData() {
     // 此方法支持跨域调用
-    $.getJSON('https://api.github.com/users/'+USER+'/repos', function (source,status) {
+    $.getJSON('https://api.github.com/users/' + USER + '/repos', function (source, status) {
         // console.log(source.data[0].id);
         console.log(status);
-        if(status === 'success'){
-            $(".loader").addClass("hidden"); // 隐藏loader
-            $(".wrapper").removeClass("hidden").addClass("animated"); // 页面载入动画
-            renderData(source);
+        if (status === 'success') {
+            projectStorage.save(source);
+            renderData(projectStorage.get());
+        } else {
+            // 错误后的提示
         }
     });
 }
+var projectStorage = {
+    save: function (source) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(source));
+    },
+    get: function () {
+        return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    }
+};
+
+function init() {
+    if (projectStorage.get() === []) {
+        getData();
+    } else {
+        renderData(projectStorage.get());
+    }
+}
+
 init();
